0a60c29823146613cf5a2f1ccc05790fa737e3cd
giyeon <bdcgy22@daum.net>
2024-05-13 18:54:33 +0900

    초기

---
+++ src/main/java/com/coro/corobackboard/CoroBackBoardApplication.java
@@ -0,0 +1,13 @@
+package com.coro.corobackboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoroBackBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoroBackBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coro/corobackboard/CoroBackBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coro.corobackboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoroBackBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
